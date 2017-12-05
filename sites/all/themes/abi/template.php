e4529902e88ace3e65b29a1e6ab2b6feabea22b5
oljasnurpeisov <oljasnurpeisov@gmail.com>
2017-12-05 23:27:41 +0600

    unset css

--- sites/all/themes/abi/template.php
+++ sites/all/themes/abi/template.php
@@ -115,3 +115,11 @@ function abi_breadcrumb($vars) {
   // Otherwise, return an empty string.
   return '';
 }
+function abi_css_alter(&$css) {
+  unset($css[drupal_get_path('module', 'system') . '/system.menus.css']);
+  unset($css[drupal_get_path('module', 'system'). '/system.base.css']);
+  unset($css[drupal_get_path('module', 'system'). '/system.messages.css']);
+  unset($css[drupal_get_path('module', 'system'). '/system.theme.css']);
+  unset($css[drupal_get_path('module', 'user') . '/user.css']);
+  unset($css[drupal_get_path('module', 'node') . '/node.css']);
+}
